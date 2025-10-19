e5b4897c32e1a07d1ef29cd4263438eb1088d8ba
h4y6 <h4y6@i.ua>
2025-10-19 23:12:11 +0300

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10287,14 +10287,17 @@ Your task is to return the word pattern for a given word. All words provided wil
 }
 
 {
+  // function barTriang(p1, p2, p3) {
+  //   return [
+  //     Math.round(((p1[0] + p2[0] + p3[0]) / 3) * 1e4) / 1e4,
+  //     Math.round(((p1[1] + p2[1] + p3[1]) / 3) * 1e4) / 1e4,
+  //   ];
+  // }
+
   function barTriang(p1, p2, p3) {
-    console.log(
-      Math.round(((p1[0] + p2[0] + p3[0]) / 3) * 1e4) / 1e4,
-      Math.round(((p1[1] + p2[1] + p3[1]) / 3) * 1e4) / 1e4
-    );
     return [
-      Math.round(((p1[0] + p2[0] + p3[0]) / 3) * 1e4) / 1e4,
-      Math.round(((p1[1] + p2[1] + p3[1]) / 3) * 1e4) / 1e4,
+      +((p1[0] + p2[0] + p3[0]) / 3).toFixed(4),
+      +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4),
     ];
   }
 
