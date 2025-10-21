b4f3c2bf000238bbb959844ab255dac5dfc84859
h4y6 <h4y6@i.ua>
2025-10-21 20:32:24 +0300

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10349,3 +10349,26 @@ Let's see some cases:
 
   // console.log(barTriang([4, 6], [12, 4], [10, 10]));
 }
+
+{
+  /**  
+7 kyu
+Sorting Dictionaries
+Python dictionaries are inherently unsorted. So what do you do if you need to sort the contents of a dictionary?
+
+Create a function that returns a sorted list of (key, value) tuples (Javascript: arrays of 2 items).
+
+The list must be sorted by the value and be sorted largest to smallest.
+Examples
+
+sortDict({3:1, 2:2, 1:3}) == [[1,3], [2,2], [3,1]]
+sortDict({1:2, 2:4, 3:6}) == [[3,6], [2,4], [1,2]]    */
+
+  function sortDict(dict) {
+    return Object.entries(dict)
+      .sort((a, b) => b[1] - a[1])
+      .map((e) => [parseInt(e[0]) ? +e[0] : e[0], e[1]]);
+  }
+
+  console.log(sortDict({ a: 2, 2: 4, 3: 6 }));
+}
