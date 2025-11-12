6a2aad2ca73ed1fc2628f279d94b7277353c8343
h4y6 <h4y6@i.ua>
2025-11-12 22:20:47 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10513,3 +10513,24 @@ capital(mixed_capitals)[1] // returns "The capital of Spain is Madrid"
 
   // console.log( capital([{ state: "Maine", capital: "Augusta" }, { country: "Spain", capital: "Madrid" }]));
 }
+
+{
+  /**  8 kyu  Enumerable Magic #20 - Cascading Subsets
+Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+each_cons([1,2,3,4], 2)
+  #=> [[1,2], [2,3], [3,4]]
+
+each_cons([1,2,3,4], 3)
+  #=> [[1,2,3],[2,3,4]]
+  
+As you can see, the lists are cascading; ie, they overlap, but never out of order. */
+
+  function eachCons(array, n) {
+    return array
+      .map((e, i, ar) => array.slice(i, i + n))
+      .filter((a) => a.length === n);
+  }
+
+  console.log(eachCons([1, 2, 3, 4], 3));
+}
