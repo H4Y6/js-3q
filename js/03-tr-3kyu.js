1a4535ff160a2bd385082d54a67c723de5554fbe
h4y6 <h4y6@i.ua>
2025-11-20 22:23:05 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10685,3 +10685,33 @@ For apples=[7,7,7,5], the output should be -1.
 
   // console.log(minSteps([1, 1, 4, 4, 8, 6]));
 }
+
+{
+  /**  6 kyu
+Change it up
+Create a function that takes a string as a parameter and does the following, in this order:
+
+Replaces every letter with the letter following it in the alphabet (see note below)
+Makes any vowels capital
+Makes any consonants lower case
+Note:
+
+the alphabet should wrap around, so Z becomes A
+in this kata, y isn't considered as a vowel.
+So, for example the string "Cat30" would return "dbU30" (Cat30 --> Dbu30 --> dbU30)   */
+
+  function changer(str) {
+    return [...str.toLowerCase()]
+      .map((e) =>
+        e === "z"
+          ? "a"
+          : e.match(/[a-z]/)
+          ? String.fromCharCode(e.charCodeAt() + 1)
+          : e
+      )
+      .map((e) => ("aeiou".includes(e) ? e.toUpperCase() : e))
+      .join("");
+  }
+
+  console.log(changer("Cat30"));
+}
