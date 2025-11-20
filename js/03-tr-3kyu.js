21a4caff6cce1f9c0687de090887008b348a7137
h4y6 <h4y6@i.ua>
2025-11-20 21:30:45 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10621,3 +10621,41 @@ That's all of your work.  */
 
   // console.log(colorOf(255, 2, 3));
 }
+
+{
+  /**  6 kyu
+Simple Fun #344: Children And Apples
+Task
+Here are some children, each with a certain number of apples. Now we have to do something to make the number of apples of each child are equal.
+
+We need to complete the redistribution step by step. With each step, we can transfer two apples from one child to another (whether or not adjacent does not matter).
+
+Given an array apples that represents the number of apples per child. Your task is to calculate the minimum step of the transition operation. If it's impossible, return -1.
+
+Example
+For apples=[7,15,9,5], the output should be 3.
+
+                       7 15 9 5
+step 1: 15-2, 5+2 ---> 7 13 9 7
+step 2: 13-2, 7+2 ---> 9 11 9 7
+step 3: 11-2, 7+2 ---> 9  9 9 9
+For apples=[7,7,7,7], the output should be 0.
+
+The children's apples are equal already. No need more step.
+
+For apples=[7,7,7,5], the output should be -1.
+
+26 apples can't be divided equally between 4 children.
+
+ */
+
+  function minSteps(apples) {
+    const med = apples.reduce((sum, e) => sum + e, 0) / apples.length;
+    if (med % 1) return -1;
+    return apples.some((e) => (Math.abs(med - e) / 2) % 1)
+      ? -1
+      : apples.reduce((score, e) => score + Math.abs(med - e) / 2, 0) / 2;
+  }
+
+  console.log(minSteps([1, 1, 4, 4, 8, 6]));
+}
