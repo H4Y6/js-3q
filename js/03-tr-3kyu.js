147e686de6bb040a6acf862ecace9f35d1addbd2
h4y6 <h4y6@i.ua>
2025-11-28 19:59:30 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10857,3 +10857,42 @@ Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l'
 
   // console.log(nerdify("Los Angeles"));
 }
+
+{
+  /**  7 kyu  Slaphead
+Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+
+You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+
+You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+
+0 hairs --> "Clean!"
+1 hair --> "Unicorn!"
+2 hairs --> "Homer!"
+3-5 hairs --> "Careless!"
+>5 hairs --> "Hobo!"
+
+So for this head: "------/------" you shoud return:
+
+["-------------", "Unicorn"]
+
+ */
+
+  function bald(x) {
+    const n = x.length - x.replaceAll("/", "").length;
+    return [
+      x.replaceAll("/", "-"),
+      n
+        ? n < 2
+          ? "Unicorn!"
+          : n < 3
+          ? "Homer!"
+          : n > 5
+          ? "Hobo!"
+          : "Careless!"
+        : "Clean!",
+    ];
+  }
+
+  console.log(bald("--/--/---/-/---"));
+}
