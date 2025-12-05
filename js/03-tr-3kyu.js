464bd299408e52d26364beea290f1e1994892d78
h4y6 <h4y6@i.ua>
2025-12-05 20:33:49 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -10967,3 +10967,50 @@ So for this head: "------/------" you shoud return:
 
   // console.log(bald("--/--/---/-/---"));
 }
+
+{
+  /**  
+7 kyu  Suzuki needs help lining up his students!
+Suzuki needs help lining up his students!
+
+Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+
+You will be given a string of student names. Sort them and return a list of names in descending order.
+
+Here is an example input:
+
+string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
+
+Here is an example return from your function:
+
+ lst = ['Takehiko',
+        'Takayuki',
+        'Takahiro',
+        'Takeshi',
+        'Takeshi',
+        'Takashi',
+        'Tadashi',
+        'Takeo',
+        'Takao']
+
+Names of equal length will be returned in reverse alphabetical order (Z->A) such that:
+
+string = "xxa xxb xxc xxd xa xb xc xd"
+
+Returns
+
+['xxd', 'xxc', 'xxb', 'xxa', 'xd', 'xc', 'xb', 'xa']           */
+
+  function lineupStudents(students) {
+    return students
+      .split(" ")
+      .sort((a, b) => b.localeCompare(a))
+      .sort((a, b) => b.length - a.length);
+  }
+
+  console.log(
+    lineupStudents(
+      "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi"
+    )
+  );
+}
