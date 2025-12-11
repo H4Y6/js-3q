2cc636bdee8624d31043441b35353a7c724b37f5
h4y6 <h4y6@i.ua>
2025-12-11 22:32:58 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11104,5 +11104,54 @@ keepOrder([1, 1, 2, 2, 2], 2) //=> 2
       .map((e) => (s = s.replace(e, "")))
       .slice(-1)[0] ?? s;
 
-  console.log(solve("abracadabra", 8));
+  // console.log(solve("abracadabra", 8));
+}
+
+{
+  /**  
+6 kyu
+Most Frequent Weekdays
+What is your favourite day of the week? Check if it's the most frequent day of the week in the year.
+
+You are given a year as integer (e. g. 2001). You should return the most frequent day(s) of the week in that year. The result has to be a list of days sorted by the order of days in week (e. g. ['Monday', 'Tuesday'], ['Saturday', 'Sunday'], ['Monday', 'Sunday']). Week starts with Monday.
+
+Input: Year as an int.
+
+Output: The list of most frequent days sorted by the order of days in week (from Monday to Sunday).
+
+Preconditions:
+
+    Week starts on Monday.
+    Year is between 1583 and 4000.
+    Calendar is Gregorian.
+
+Examples (input -> output):
+
+* 2427 -> ['Friday']
+* 2185 -> ['Saturday']
+* 2860 -> ['Thursday', 'Friday']     */
+
+  function mostFrequentDays(year) {
+    const week = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const [first, last] = [
+      new Date(year, 0, 1).getDay(),
+      new Date(year + 1, 0, 0).getDay(),
+    ];
+
+    return first === last
+      ? [week[first]]
+      : !first
+      ? [week[last], week[first]]
+      : [week[first], week[last]];
+  }
+
+  console.log(mostFrequentDays(1984));
 }
