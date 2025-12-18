3421968ed623d3a4054fc7e98c6af7572bc3a0dc
h4y6 <h4y6@i.ua>
2025-12-18 21:57:21 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11311,3 +11311,15 @@ Examples (input -> output):
 
   // console.log(mostFrequentDays(1984));
 }
+
+{
+  /** Implement a function which filters out array values which satisfy the given predicate.
+
+reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]   */
+
+  const reject = (array, predicate) => {
+    return array.filter((e) => !predicate(e));
+  };
+
+  console.log(reject(["a", "b", 3, "d"], (x) => typeof x === "number"));
+}
