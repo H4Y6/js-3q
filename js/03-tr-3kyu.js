c65ad196b5f096701b822ad46edfaee6771a47ba
h4y6 <h4y6@i.ua>
2025-12-18 23:49:37 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11381,12 +11381,14 @@ if (x) == ['a', 3]  you should return [['a', 3], ['a', 3], ['a', 3]].    */
   //     : "Void!";
   // }
 
+  // const explode = (x) => x.some((e) => +(typeof e === "number")) ? Array(x.reduce((s, e) => (typeof e === "number" ? s + e : s), 0)).fill(x ) : "Void!";
+
   const explode = (x) =>
-    x.some((e) => +(typeof e === "number"))
+    new String(x[0] + x[1]).replace(/\D/g, "")
       ? Array(x.reduce((s, e) => (typeof e === "number" ? s + e : s), 0)).fill(
           x
         )
       : "Void!";
 
-  console.log(explode(["y", 0]));
+  // console.log(explode(["y", 0]));
 }
