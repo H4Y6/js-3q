dee185dda3a5c2949f46b361a8f08761d216587e
h4y6 <h4y6@i.ua>
2025-12-31 15:08:04 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11606,12 +11606,34 @@ Either teams score has a range of 0-9, and the ref won't say the same string eve
 }
 
 
-{
-  function solve(s, g) {
+{  /**  
+7 kyu
+GCD sum
+Given the sum and gcd of two numbers, return those two numbers in ascending order. If the numbers do not exist, return -1, (or NULL in C, tuple (-1,-1) in C#, pair (-1,-1) in C++,None in Rust, array {-1,-1} in Java and Golang).
 
-    const second = s - g
-    if (second % g) { return -1 }
-    else { return [g, second] }
+For example: 
+Given sum = 12 and gcd = 4...
+
+solve(12,4) = [4,8]. The two numbers 4 and 8 sum to 12 and have a gcd of 4.
+
+solve(12,5) = -1. No two numbers exist that sum to 12 and have gcd of 5.
+
+solve(10,2) = [2,8]. Note that [4,6] is also a possibility but we pick the one with the lower first element: 2 < 4, so we take [2,8].
+
+More examples in test cases.  */
+
+  // function solve(s, g) {
+
+  //   const second = s - g
+  //   if (second % g) { return -1 }
+  //   else { return [g, second] }
+  // }
+
+  const solve = (s, g) => {
+    if ((s - g) % g) { return -1 }
+    else { return [g, s - g] }
   }
+
+
   console.log(solve(12, 4))
 }
