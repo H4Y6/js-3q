0cd70ac427d58db531a05129ce539736ebd30303
h4y6 <h4y6@i.ua>
2025-12-31 23:07:41 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11657,9 +11657,12 @@ If the input is 78, then you should return "01:18", because 78 minutes converts 
   //   return num >= 0 ? ((Math.floor(num / 60) + '').length < 2 ? ('00' + Math.floor(num / 60)).slice(-2) : Math.floor(num / 60)) + ':' + ('00' + num % 60).slice(-2) : '00:00'
   // }
 
-  function timeConvert(num) {
-    return num > 0 ? ('' + Math.floor(num / 60)).padStart(2, '0') + ':' + ('' + num % 60).padStart(2, '0') : '00:00'
-  }
+  // function timeConvert(num) {
+  //   return num > 0 ? ('' + Math.floor(num / 60)).padStart(2, '0') + ':' + ('' + num % 60).padStart(2, '0') : '00:00'
+  // }
+
+
+  const timeConvert = (num) => num > 0 ? ('' + Math.floor(num / 60)).padStart(2, '0') + ':' + ('' + num % 60).padStart(2, '0') : '00:00'
 
   console.log(timeConvert(565757))
 }
