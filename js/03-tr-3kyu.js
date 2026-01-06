bb1b70b560c4d98e69811be2620064e6c08db039
h4y6 <h4y6@i.ua>
2026-01-07 00:21:32 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11796,28 +11796,54 @@ should return "unknown value"
   // console.log(getNaughtyNames(people))
 }
 
-{
-  function compareVersions(version1, version2) {
-    console.log(version1, '"', version2)
-    let v1 = version1.split('.')
-    let v2 = version2.split('.')
+{ /**  
+6 kyu
+Compare Versions
+Karan's company makes software that provides different features based on the version of operating system of the user.
 
-    if (v1.length - v2.length) {
-      if (v1.length > v2.length) {
-        v2.push('0'.repeat(v1.length - v2.length).split(''));
-        v2 = v2.flatMap(e => e)
-      } else {
-        v1.push('0'.repeat(v2.length - v1.length).split(''));
-        v1 = v1.flatMap(e => e)
-      }
+To compare versions, Karan currently parses both version numbers as floats.
+
+While this worked for OS versions 10.6, 10.7, 10.8 and 10.9, the operating system company just released OS version 10.10. This causes his method to fail, as 10.9 is greater than 10.10 when interpreting both as numbers, despite being a lesser version.
+
+Help Karan by writing him a function which compares two versions, and returns whether or not the first one is greater than or equal to the second.
+
+Specification notes:
+
+    Versions are provided as strings of one or more integers separated by ..
+    The version strings will never be empty.
+    The two versions are not guaranteed to have an equal amount of sub-versions, when this happens assume that all missing sub-versions are zero.
+    Two versions which differ only by trailing zero sub-versions will never be given.   */
+
+  // function compareVersions(version1, version2) {
+  //   console.log(version1, '"', version2)
+  //   let v1 = version1.split('.')
+  //   let v2 = version2.split('.')
+
+  //   if (v1.length - v2.length) {
+  //     if (v1.length > v2.length) {
+  //       v2.push('0'.repeat(v1.length - v2.length).split(''));
+  //       v2 = v2.flatMap(e => e)
+  //     } else {
+  //       v1.push('0'.repeat(v2.length - v1.length).split(''));
+  //       v1 = v1.flatMap(e => e)
+  //     }
+  //   }
+
+  //   for (let i = 0; i < v1.length; i++) {
+  //     if (parseInt(v1[i]) - parseInt(v2[i])) return parseInt(v1[i]) > parseInt(v2[i])
+  //   }
+
+  //   return true
+  // }
+
+  const compareVersions = (version1, version2) => {
+    const v1 = version1.split('.').map(Number);
+    const v2 = version2.split('.').map(Number);
+    for (let i = 0; i < Math.max(v1.length, v2.length); i++) {
+      if ((v1[i] ?? 0) - (v2[i] ?? 0)) return (v1[i] ?? 0) > (v2[i] ?? 0)
     }
+    return true;
+  };
 
-    for (let i = 0; i < v1.length; i++) {
-      if (parseInt(v1[i]) - parseInt(v2[i])) return parseInt(v1[i]) > parseInt(v2[i])
-    }
-
-    return true
-  }
-
-  console.log(compareVersions("10.4.3.10", "10.4.3.9"))
+  // console.log(compareVersions("10.4.3.10", "10.4.3.9"))
 }
