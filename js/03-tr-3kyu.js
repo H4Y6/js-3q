9fa9383b90a463eb1135f689ff823325741f034c
h4y6 <h4y6@i.ua>
2026-01-22 23:29:07 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -11984,3 +11984,29 @@ Random input limits:
 
   // console.log(duckDuckGoose(players, 5))
 }
+
+{  /**  
+7 kyu
+Do you speak retsec?
+
+You and your friends want to play undercover agents. In order to exchange your secret messages, you've come up with the following system: you take the word, cut it in half, and place the first half behind the latter. If the word has an uneven number of characters, you leave the middle at its previous place:
+
+retsec examples
+
+That way, you'll be able to exchange your messages in private.
+Task
+
+You're given a single word. Your task is to swap the halves. If the word has an uneven length, leave the character in the middle at that position and swap the chunks around it.
+Examples
+
+reverseByCenter("secret")  == "retsec" // no center character
+reverseByCenter("agent")   == "nteag"  // center character is "e"
+ */
+
+  function reverseByCenter(s) {
+    return s.length % 2 ? s.slice(s.length / 2 + 1) + s.slice(s.length / 2, s.length / 2 + 1) + s.slice(0, s.length / 2) : s.slice(s.length / 2)
+      + s.slice(0, s.length / 2)
+  }
+
+  console.log(reverseByCenter('agenda'))
+}
