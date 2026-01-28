23fed26eca649f82a4fbdfc17a79c24ef7a48eeb
h4y6 <h4y6@i.ua>
2026-01-28 16:19:59 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12033,3 +12033,28 @@ The function should return false if the array is empty.  */
 
   // console.log(any([1, 2, 3, 4], function (v, i) { return v > 1 }))
 }
+
+{  /**  
+7 kyu
+Eliminate the intruders! Bit manipulation
+You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+In practice, you should implement this function:
+
+function eliminateUnsetBits(number);
+
+Examples
+
+eliminateUnsetBits("11010101010101") ->  255 (= 11111111)
+eliminateUnsetBits("111") ->  7
+eliminateUnsetBits("1000000") -> 1
+eliminateUnsetBits("000") -> 0
+ */
+
+  function eliminateUnsetBits(number) {
+    number = number.replace(/0/g, '')
+    return number.length ? parseInt(number, 2) : 0
+  }
+
+  console.log(eliminateUnsetBits("000"))
+}
