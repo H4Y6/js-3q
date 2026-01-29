833cc2a83817a5159fb3bf102d99621a8d8dc39f
h4y6 <h4y6@i.ua>
2026-01-29 13:38:33 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12070,5 +12070,69 @@ eliminateUnsetBits("000") -> 0
 
   const eliminateUnsetBits = (number) => (n => Number.isNaN(n) ? 0 : n)(parseInt(number.replace(/0/g, ''), 2))
 
-  console.log(eliminateUnsetBits("000"))
+  // console.log(eliminateUnsetBits("000"))
+}
+
+{  /**  6 kyu  Irreducible Sum of Rationals
+You will have a list of rationals in the form
+lst = [ [numer_1, denom_1] , ... , [numer_n, denom_n] ]
+or
+lst = [ (numer_1, denom_1) , ... , (numer_n, denom_n) ]
+where all numbers are positive integers. You have to produce their sum N / D in an irreducible form: this means that N and D have only 1 as a common divisor.
+Return the result in the form:
+
+    [N, D] in Ruby, Crystal, Python, Clojure, JS, CS, PHP, Julia, Pascal
+    Just "N D" in Haskell, PureScript
+    "[N, D]" in Java, CSharp, TS, Scala, PowerShell, Kotlin
+    "N/D" in Go, Nim
+    {N, D} in C, C++, Elixir, Lua
+    Some((N, D)) in Rust
+    Some "N D" in F#, Ocaml
+    c(N, D) in R
+    (N, D) in Swift
+    '(N D) in Racket
+
+If the result is an integer (D evenly divides N) return:
+
+    an integer in Ruby, Crystal, Elixir, Clojure, Python, JS, CS, PHP, R, Julia
+    Just "n" (Haskell, PureScript)
+    "n" Java, CSharp, TS, Scala, PowerShell, Go, Nim, Kotlin
+    {n, 1} in C, C++, Lua
+    Some((n, 1)) in Rust
+    Some "n" in F#, Ocaml,
+    (n, 1) in Swift
+    n in Racket
+    (n, 1) in Swift, Pascal, Perl
+
+If the input list is empty, return
+
+    nil/None/null/Nothing
+    {0, 1} in C, C++, Lua
+    "0" in Scala, PowerShell, Go, Nim
+    O in Racket
+    "" in Kotlin
+    [0, 1] in C++, "[0, 1]" in Pascal
+    [0, 1] in Perl
+
+Example:
+
+[ [1, 2], [1, 3], [1, 4] ]  -->  [13, 12]
+1/2  +  1/3  +  1/4     =      13/12   */
+
+  function sumFracts(l) {
+    if (!l.length) return null
+    let cd = l.reduce((d, e) => d * e[1], 1)
+    let sum = l.reduce((s, e) => s + e[0] * cd / e[1], 0)
+
+    for (let i = 2; i < cd; i++) {
+      while (cd % i === 0 && sum % i === 0) {
+        sum /= i
+        cd /= i
+      }
+    }
+
+    return sum % cd === 0 ? sum / cd : [sum, cd]
+  }
+
+  console.log(sumFracts([[2, 7], [1, 3], [1, 12]]))
 }
