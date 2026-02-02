a466358284d51d9b6577b25c09137d7bdde4273b
h4y6 <h4y6@i.ua>
2026-02-02 09:41:31 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12412,5 +12412,42 @@ output: "StOp mAkInG SpOnGeBoB MeMeS!"   */
   const spongeMeme = (sentence) =>
     sentence.replace(/./g, (e, i) => e[`to${i % 2 ? `Low` : `Upp`}erCase`]());
 
-  console.log(spongeMeme("stop Making spongebob Memes!"));
+  // console.log(spongeMeme("stop Making spongebob Memes!"));
+}
+
+{
+  /**  7 kyu  Return a sorted list of objects
+You'll be passed an array of objects (list) - you must sort them in descending order based on the value of the specified property (sortBy).
+
+Example
+When sorted by "a", this:
+
+[
+  {"a": 1, "b": 3},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 4, "b": 12}
+]
+should return:
+
+[
+  {"a": 4, "b": 12},
+  {"a": 3, "b": 2},
+  {"a": 2, "b": 40},
+  {"a": 1, "b": 3}
+]
+The values will always be numbers, and the properties will always exist.*/
+
+  function sortList(sortBy, list) {
+    return list.sort((c, d) => d[sortBy] - c[sortBy]);
+  }
+
+  console.log(
+    sortList("a", [
+      { a: 1, b: 3 },
+      { a: 3, b: 2 },
+      { a: 2, b: 40 },
+      { a: 4, b: 12 },
+    ]),
+  );
 }
