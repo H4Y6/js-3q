0b22242e93b8fcc689c24ac8fc23a0486310cd1b
h4y6 <h4y6@i.ua>
2026-02-04 14:53:28 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12444,12 +12444,37 @@ The values will always be numbers, and the properties will always exist.*/
 
   const sortList = (sortBy, list) => list.sort((c, d) => d[sortBy] - c[sortBy]);
 
-  console.log(
-    sortList("a", [
-      { a: 1, b: 3 },
-      { a: 3, b: 2 },
-      { a: 2, b: 40 },
-      { a: 4, b: 12 },
-    ]),
-  );
+  // console.log(sortList("a", [{ a: 1, b: 3 }, { a: 3, b: 2 }, { a: 2, b: 40 }, { a: 4, b: 12 }]));
+}
+
+{
+  /**  7 kyu
+Complete The Pattern #4
+You have to write a function pattern which creates the following pattern upto n number of rows. If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+Examples:
+pattern(4):
+1234
+234
+34
+4
+pattern(6):
+123456
+23456
+3456
+456
+56
+6
+Note: There are no blank spaces
+
+Hint: Use \n in string to jump to next line */ function pattern(n) {
+    if (n < 1) return "";
+    return [...Array(n + 1).keys()]
+      .reduce(
+        (arr, e, i, ar) => [...arr, ar.slice(i + 1).join("") + "\n"].join(""),
+        [],
+      )
+      .slice(0, -2);
+  }
+
+  console.log(pattern(2));
 }
