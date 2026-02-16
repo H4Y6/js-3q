43d897aded93c337ede53442858e2bb2172ead41
h4y6 <h4y6@i.ua>
2026-02-16 22:13:23 +0200

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12717,13 +12717,30 @@ The answer should be rounded to the nearest 2 decimal places.    */
 }
 
 {
-  function capitalsFirst(str) {
-    return ((s) =>
+  /**  7 kyu
+Capitals first!
+Create a function that takes an input String and returns a String, where all the uppercase words of the input String are in front and all the lowercase words at the end. The order of the uppercase and lowercase words should be the order in which they occur.
+
+If a word starts with a number or special character, skip the word and leave it out of the result.
+
+Input String will not be empty.
+
+For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me" */
+
+  // function capitalsFirst(str) {
+  //   return ((s) =>
+  //     s
+  //       .filter((e) => e[0] === e[0].toUpperCase())
+  //       .concat(s.filter((e) => e[0] === e[0].toLowerCase()))
+  //       .join(" "))(str.split(" ").filter((e) => /[a-z]/i.test(e[0])));
+  // }
+
+  const capitalsFirst = (str) =>
+    ((s) =>
       s
         .filter((e) => e[0] === e[0].toUpperCase())
         .concat(s.filter((e) => e[0] === e[0].toLowerCase()))
         .join(" "))(str.split(" ").filter((e) => /[a-z]/i.test(e[0])));
-  }
 
   console.log(capitalsFirst("hey You, 33 Sort me Already!"));
 }
