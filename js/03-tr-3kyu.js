38c463b4cbd8876ddd29bd2a10f16591f1e9a3a2
h4y6 <h4y6@i.ua>
2026-04-06 20:53:40 +0300

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12895,3 +12895,17 @@ Examples
 
   // console.log(orderWord("null"));
 }
+
+{
+  /**  7 kyu  Word to binary
+Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+
+Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+ */
+
+  function wordToBin(str) {
+    return [...str].map((e) => e.charCodeAt().toString(2).padStart(8, "0"));
+  }
+
+  console.log(wordToBin("man"));
+}
