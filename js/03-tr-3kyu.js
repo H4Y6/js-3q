b7c3777dff5326e42aea155882d3e694a05fcffa
h4y6 <h4y6@i.ua>
2026-05-07 23:32:27 +0300

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12924,11 +12924,13 @@ Write a function that returns only the decimal part of the given number.
 You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
 Examples
 getDecimal(2.4)  === 0.4
-getDecimal(-0.2) === 0.2
- */
-  function getDecimal(n) {
-    return n % 1 ? +(0 + n.toString().match(/\.\d+/)[0]) : 0;
-  }
+getDecimal(-0.2) === 0.2 */
+
+  // function getDecimal(n) {
+  //   return n % 1 ? +(0 + n.toString().match(/\.\d+/)[0]) : 0;
+  // }
+
+  const getDecimal = (n) => (n % 1 ? +(0 + n.toString().match(/\.\d+/)[0]) : 0);
 
   console.log(getDecimal(11));
 }
