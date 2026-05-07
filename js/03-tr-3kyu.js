2cabc75872e13475d092cb406c6fe5f37dcac4e2
h4y6 <h4y6@i.ua>
2026-05-07 23:28:43 +0300

    modified:   js/03-tr-3kyu.js

--- js/03-tr-3kyu.js
+++ js/03-tr-3kyu.js
@@ -12915,3 +12915,20 @@ Example: 'man' should return [ '01101101', '01100001', '01101110' ]
 
   // console.log(wordToBin("man"));
 }
+
+{
+  /**  
+7 kyu  Get decimal part of the given number
+Write a function that returns only the decimal part of the given number.
+
+You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+Examples
+getDecimal(2.4)  === 0.4
+getDecimal(-0.2) === 0.2
+ */
+  function getDecimal(n) {
+    return n % 1 ? +(0 + n.toString().match(/\.\d+/)[0]) : 0;
+  }
+
+  console.log(getDecimal(11));
+}
